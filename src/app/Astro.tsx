adfcf3df660769028a7e512d8f827fe5ba529d46
Gediminas <gediminas.kaminskas@kilo.health>
2024-03-28 16:14:26 +0200

    draft

--- src/app/Astro.tsx
+++ src/app/Astro.tsx
@@ -1,23 +1,9 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 import { Origin, Horoscope } from "circular-natal-horoscope-js";
 import Chart from "@astrodraw/astrochart";
 
-// {
-//     "name": "Mario Rossi",
-//     "year": 1991,
-//     "month": 5,
-//     "day": 3,
-//     "hour": 16,
-//     "minute": 30,
-//     "longitude": 51.5058800846886,
-//     "latitude": -0.12564233344218367,
-//     "city": "London",
-//     "timezone": "Europe/London",
-//     "language": "EN"
-// }
-//
 const origin = new Origin({
   year: 1991,
   month: 4, // 0 = January, 11 = December!
@@ -27,35 +13,23 @@ const origin = new Origin({
   latitude: 51.5058800846886,
   longitude: -0.12564233344218367,
 });
-//
+
 const horoscope = new Horoscope({
   origin,
-  houseSystem: "whole-sign",
-  zodiac: "tropical",
-  aspectPoints: ["bodies", "points", "angles"],
-  aspectWithPoints: ["bodies", "points", "angles"],
-  aspectTypes: ["major", "minor"],
-  customOrbs: {},
-  language: "en",
 });
 
-function moveElementToEnd(arr: any[]) {
-  // Remove the first element from the array and store it in a variable
-  const firstElement = arr.shift();
-
-  // Push the first element to the end of the array
-  arr.push(firstElement);
-
-  return arr; // Return the modified array
-}
-
 const Astro = () => {
-  //
   const planets = horoscope.CelestialBodies;
-
   const cusps = horoscope.ZodiacCusps.map((c) => {
-    return c.ChartPosition.Horizon.DecimalDegrees;
+    const degree = c.ChartPosition.Ecliptic.DecimalDegrees;
+    let result = degree - 180;
+    if (result < 0) {
+      result += 360;
+    }
+    return result;
   });
+
+  console.log(cusps);
 
   var chart = new Chart("paper", 800, 800, {
     COLOR_GEMINI: "white",
@@ -104,9 +78,8 @@ const Astro = () => {
     // cusps: cusps.reverse(),
     // cusps: [296, 350, 30, 56, 75, 94, 116, 170, 210, 236, 255, 274],
   };
-  //
-  console.log(cusps);
-  console.log([296, 350, 30, 56, 75, 94, 116, 170, 210, 236, 255, 274]);
+
+  //   console.log(cusps);
   const chart2 = new Chart("paper2", 800, 800);
   const data2 = {
     planets: {
@@ -121,7 +94,7 @@ const Astro = () => {
       Mercury: [312],
       Venus: [330],
     },
-    cusps: [296, 350, 30, 56, 75, 94, 116, 170, 210, 236, 255, 274],
+    cusps: [360, 350, 340, 320, 310, 300, 290, 280, 270, 260, 250, 240],
   };
 
   //   chart.radix(data2);
